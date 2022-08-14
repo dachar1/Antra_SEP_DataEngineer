1591258ea73789de13e8e3c34b4ce88068e28c3f
dachar1 <acharya.david@outlook.com>
2022-08-14 19:24:50 -0400

    Delete SQLQuery_6.sql

--- SQLQuery_6.sql
+++
@@ -1,7 +0,0 @@
-SELECT Warehouse.StockItems.StockItemName, Warehouse.StockItemTransactions.TransactionOccurredWhen, Application.StateProvinces.StateProvinceName
-FROM ((Warehouse.StockItems
-INNER JOIN Warehouse.StockItemTransactions ON Warehouse.StockItems.StockItemID = Warehouse.StockItemTransactions.StockItemID)
-INNER JOIN Application.StateProvinces ON Warehouse.StockItems.LastEditedBY = Application.StateProvinces.LastEditedBY)
-WHERE YEAR(TransactionOccurredWhen)='2014' 
-AND NOT Application.StateProvinces.StateProvinceName  ='Alabama'
-AND NOT Application.StateProvinces.StateProvinceName  ='Georgia'
