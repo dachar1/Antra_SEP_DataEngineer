035850780d23546923eae3030073e6450e602b9f
dachar1 <acharya.david@outlook.com>
2022-08-14 19:24:44 -0400

    Delete SQLQuery_4.sql

--- SQLQuery_4.sql
+++
@@ -1,5 +0,0 @@
-SELECT Warehouse.StockItems.StockItemName,Warehouse.StockItems.QuantityPerOuter
-FROM Warehouse.StockItems
-INNER JOIN
-Purchasing.PurchaseOrders ON Warehouse.StockItems.SupplierID=Purchasing.PurchaseOrders.SupplierID
-WHERE YEAR(OrderDate) ='2013'
