f134f9b6728f41a7a3e304a9a2879a858e0d31fa
dachar1 <acharya.david@outlook.com>
2022-08-14 19:24:52 -0400

    Delete SQLQuery_7.sql

--- SQLQuery_7.sql
+++
@@ -1,5 +0,0 @@
-SELECT StateProvinceName, OrderDate , 
-DateDiff(Day, OrderDate, ExpectedDeliveryDate) As Average_DeliveryDate
-from Sales.Orders 
-INNER JOIN Sales.Customers ON Sales.Orders.CustomerID = Sales.Customers.CustomerID
-INNER JOIN Application.StateProvinces ON Sales.Orders.LastEditedBY = Application.StateProvinces.LastEditedBY;
