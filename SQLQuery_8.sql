fe5f629d4b27732f5fa86827688d8374d8aba148
dachar1 <acharya.david@outlook.com>
2022-08-14 19:24:55 -0400

    Delete SQLQuery_8.sql

--- SQLQuery_8.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT StateProvinceName, DateDiff(MONTH, OrderDate, ExpectedDeliveryDate) As Average_DeliveryMonth
-FROM Sales.Orders 
-INNER JOIN Sales.Customers ON Sales.Orders.CustomerID = Sales.Customers.CustomerID
-INNER JOIN Application.StateProvinces ON Sales.Orders.LastEditedBY = Application.StateProvinces.LastEditedBY
-ORDER BY StateProvinceName ASC;
